2f977666d3ddae6208e0ec996378d43d5f16ff48
Christina Liu <cl3968@nyu.edu>
2020-04-08 22:12:02 -0400

    m2 done

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 require('./db');
 
+const mongoose = require('mongoose');
+const Resturant = mongoose.model('Resturant');
+const Review = mongoose.model('Review');
+
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -25,8 +30,117 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+	// res.locals.user = req.user;
+	// res.locals.authenticated = ! req.user.anonymous;
+	if (req.session.count === undefined){
+		req.session.count =0;
+		res.locals.count =0;
+	}
+	else {
+		req.session.count +=1;
+		res.locals.count = req.session.count;
+	}
+	
+	next();
+});
+
 app.get('/', (req, res) => {
-  res.render('index');
+
+	if (req.query.name===undefined && req.query.cuisine===undefined && req.query.borough===undefined && req.query.price===undefined){
+		Resturant.find({}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+	}
+
+	else if(req.query.name!=="" && req.query.cuisine!=="" && req.query.borough!=="" && req.query.price!==""){
+		Resturant.find({name: req.query.name, cuisine: req.query.cuisine, borough: req.query.borough, price: req.query.price}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+	}
+	else if(req.query.name!=="" && req.query.borough!=="" && req.query.price!==""){
+		Resturant.find({name: req.query.name, borough: req.query.borough, price: req.query.price}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+
+	}
+	else if(req.query.name!=="" && req.query.cuisine!=="" && req.query.price!==""){
+		Resturant.find({name: req.query.name, cuisine: req.query.cuisine, price: req.query.price}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+
+	}
+	else if(req.query.borough!=="" && req.query.cuisine!=="" && req.query.price!==""){
+		Resturant.find({borough: req.query.borough, cuisine: req.query.cuisine, price: req.query.price}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+
+	}
+	else if(req.query.name!=="" && req.query.borough!==""){
+		Resturant.find({name: req.query.name, borough: req.query.borough}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+
+	}
+	else if(req.query.name!=="" && req.query.cuisine!==""){
+		Resturant.find({name: req.query.name, cuisine: req.query.cuisine}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+
+	}
+	else if(req.query.name!=="" && req.query.price!==""){
+		Resturant.find({name: req.query.name, price: req.query.price}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+
+	}
+	else if(req.query.cuisine!=="" && req.query.borough!==""){
+		Resturant.find({cuisine: req.query.cuisine, borough: req.query.borough}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+
+	}
+	else if(req.query.cuisine!=="" && req.query.price!==""){
+		Resturant.find({cuisine: req.query.cuisine, price: req.query.price}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+
+	}
+	else if(req.query.price!=="" && req.query.borough!==""){
+		Resturant.find({price: req.query.price, borough: req.query.borough}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+
+	}
+	else if(req.query.name!==""){
+		Resturant.find({name: req.query.name}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+
+	}
+	else if(req.query.cuisine!==""){
+		Resturant.find({cuisine: req.query.cuisine}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+			
+	}
+	else if(req.query.borough!==""){
+		Resturant.find({borough: req.query.borough}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+	}
+	else if(req.query.price!==""){
+		Resturant.find({price: req.query.price}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+	}
+	else {
+		Resturant.find({}, (err, result) => {
+			res.render('index', {resturants: result});
+		});
+	}
+	
+	
 });
 
 app.listen(3000);
--- db.js
+++ db.js
@@ -12,25 +12,22 @@ const User = new mongoose.Schema({
 });
 // reviews
 // * each review must have a related user
-// * users can also optionally rank a resturant in their review 
 // * they also have a date when this review was posted
 const Review = new mongoose.Schema({
   username: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
   verdict:{type: String, required: true},
-  rank: String,
-  createdAt: {type: Date, required: true}
+  createdAt: {type: Date, default: Date.now, required: true}
 });
 // resturant
 // * each resturant has a name, cuisine associated with it,
-// * borough, price point, popularity, and pictures
+// * borough, price point, and pictures
 // * users can post reviews
 const Resturant = new mongoose.Schema({
   name: {type: String, required: true},
   cuisine: {type: String, required: true},
   borough: {type: String, required: true},
   price: {type: String, required: true},
-  popularity: {type: Number, required: true},
-  pictures: {type: Image, required: true},
+  // pictures: {type: Image, required: true},
   reviews: [Review]
 });
 
@@ -38,4 +35,4 @@ mongoose.model('User', User);
 mongoose.model('Review', Review);
 mongoose.model('Resturant', Resturant);
 
-mongoose.connect('mongodb://localhost/finalproject', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/final', {useNewUrlParser: true, useUnifiedTopology: true});
